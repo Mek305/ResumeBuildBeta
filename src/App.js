0341f80ce43cde823ef679f186c6b84dd45409b5
Mek305 <Mek305@gmail.com>
2019-11-25 10:45:21 -0500

    Resume Builder Beta Mode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
+import GetStarted from './Components/Getstarted.js';
+import Navi from './Components/Navi.js';
+import Templates from './Components/Template.js';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+   return (
+     <Router>
+       <div className ="App">
+     <Navi />
+       <Switch>
+         <Route path ='/' exact component ={Home} />
+         <Route path ='/getstarted' component = {GetStarted} />
+         <Route path ='/templates' component = {Templates} />
+       </Switch>
+       
+     </div>
+     </Router>
+   )
+   
+ }
+
+ const Home = () =>(
+   <div className ="head">
+     <h1>Welcome to ResumeBuider Beta!!!</h1>
+​
+     <Link to="/getstarted">
+       <button>Build Your Resume Now!!!</button>
+     </Link>
+   <hr></hr>
+   <h1>Just click to insert custom pre-written bullets</h1>
+     <p>Save time with thousands of pre-written “smart bullets” that you can click to plug-in to your resume.</p>
+     <Link to="/getstarted">
+       <button>Build Your Resume Now!!!</button>
+     </Link>
+  
+   </div>
+   
+  
+     
+
+ )
 
 export default App;
